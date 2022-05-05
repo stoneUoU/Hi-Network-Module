53620973ffa0246541e66962b50b2919adc8f2ba
linlei <stoneuou@163.com>
2022-05-05 10:53:19 +0800

    [feat]1.A newest Project for Hi-Network-Module

---
+++ Example/Pods/Target Support Files/Hi-Network-Module/Hi-Network-Module-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Hi_Network_Module : NSObject
+@end
+@implementation PodsDummy_Hi_Network_Module
+@end
---
+++ Example/Pods/Target Support Files/Pods-Hi-Network-Module_Example/Pods-Hi-Network-Module_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Hi_Network_Module_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Hi_Network_Module_Example
+@end
